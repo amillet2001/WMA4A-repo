89c537b7f94cd4b3aa4830233cdb7c981ed3a832
Carl Rham Jhay <dayaocarlrhamjhay@gmail.com>
2025-09-23 15:25:34 +0800

    i will commit this

--- DAYAO.js
+++ DAYAO.js
@@ -1,16 +1,13 @@
 const toggleBtn = document.getElementById("theme-toggle");
 const html = document.documentElement;
 
-// Load saved theme from localStorage
 if (localStorage.getItem("theme") === "dark") {
   html.classList.add("dark");
 }
 
-// Toggle on click
 toggleBtn.addEventListener("click", () => {
   html.classList.toggle("dark");
 
-  // Save preference
   if (html.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
   } else {
