96fe609063b510f895f53d3dcb4464a127ba8696
NESHREN <neshrennn@gmail>
2025-09-23 15:20:41 +0800

    first major commit on js

--- SULTAN.js
+++ SULTAN.js
@@ -5,7 +5,7 @@ const form = document.querySelector("form"),
   passInput = passField.querySelector(".password"),
   cPassField = form.querySelector(".confirm-password"),
   cPassInput = cPassField.querySelector(".cPassword");
-//Email Validtion
+//Email Validtion 
 function checkEmail() {
   const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   if (!emailInput.value.match(emaiPattern)) {
